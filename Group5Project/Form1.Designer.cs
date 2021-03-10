e935511f545e80a95a050c955e417ca13cea23c4
hieuttblackpink <trantronghieu369963@gmail.com>
2021-03-10 15:28:33 +0700

    Add project files.

---
+++ Group5Project/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿
+namespace Group5Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNgaysinh = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDiemtb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGioitinh = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMakhoa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHo = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.findBtn = new System.Windows.Forms.Button();
+            this.listMSSV = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1358, 489);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.MistyRose;
+            this.panel5.Controls.Add(this.dataGridView1);
+            this.panel5.Location = new System.Drawing.Point(426, 104);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(929, 382);
+            this.panel5.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LavenderBlush;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(880, 271);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.MistyRose;
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Location = new System.Drawing.Point(426, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(929, 95);
+            this.panel4.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(110)))), ((int)(((byte)(110)))));
+            this.label7.Location = new System.Drawing.Point(18, 11);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(890, 73);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "KẾT QUẢ HỌC TẬP";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MistyRose;
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.txtTen);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.txtNgaysinh);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.txtDiemtb);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.txtGioitinh);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.txtMakhoa);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.txtHo);
+            this.panel3.Location = new System.Drawing.Point(3, 104);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(417, 382);
+            this.panel3.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label6.Location = new System.Drawing.Point(11, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 30);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Tên SV";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtTen
+            // 
+            this.txtTen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.txtTen.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTen.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTen.Location = new System.Drawing.Point(137, 87);
+            this.txtTen.Multiline = true;
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(262, 30);
+            this.txtTen.TabIndex = 10;
+            this.txtTen.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label5.Location = new System.Drawing.Point(11, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 30);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ngày sinh";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtNgaysinh
+            // 
+            this.txtNgaysinh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.txtNgaysinh.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNgaysinh.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNgaysinh.Location = new System.Drawing.Point(137, 135);
+            this.txtNgaysinh.Multiline = true;
+            this.txtNgaysinh.Name = "txtNgaysinh";
+            this.txtNgaysinh.Size = new System.Drawing.Size(262, 30);
+            this.txtNgaysinh.TabIndex = 8;
+            this.txtNgaysinh.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label4.Location = new System.Drawing.Point(11, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 30);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Điểm TB";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtDiemtb
+            // 
+            this.txtDiemtb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.txtDiemtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDiemtb.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiemtb.Location = new System.Drawing.Point(137, 279);
+            this.txtDiemtb.Multiline = true;
+            this.txtDiemtb.Name = "txtDiemtb";
+            this.txtDiemtb.Size = new System.Drawing.Size(262, 30);
+            this.txtDiemtb.TabIndex = 6;
+            this.txtDiemtb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label3.Location = new System.Drawing.Point(11, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Giới tính";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtGioitinh
+            // 
+            this.txtGioitinh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.txtGioitinh.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtGioitinh.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGioitinh.Location = new System.Drawing.Point(137, 184);
+            this.txtGioitinh.Multiline = true;
+            this.txtGioitinh.Name = "txtGioitinh";
+            this.txtGioitinh.Size = new System.Drawing.Size(262, 30);
+            this.txtGioitinh.TabIndex = 4;
+            this.txtGioitinh.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label2.Location = new System.Drawing.Point(11, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mã khoa";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtMakhoa
+            // 
+            this.txtMakhoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.txtMakhoa.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMakhoa.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMakhoa.Location = new System.Drawing.Point(137, 231);
+            this.txtMakhoa.Multiline = true;
+            this.txtMakhoa.Name = "txtMakhoa";
+            this.txtMakhoa.Size = new System.Drawing.Size(262, 30);
+            this.txtMakhoa.TabIndex = 2;
+            this.txtMakhoa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(11, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Họ SV";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtHo
+            // 
+            this.txtHo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.txtHo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtHo.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHo.Location = new System.Drawing.Point(137, 39);
+            this.txtHo.Multiline = true;
+            this.txtHo.Name = "txtHo";
+            this.txtHo.Size = new System.Drawing.Size(262, 30);
+            this.txtHo.TabIndex = 0;
+            this.txtHo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MistyRose;
+            this.panel2.Controls.Add(this.findBtn);
+            this.panel2.Controls.Add(this.listMSSV);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(417, 95);
+            this.panel2.TabIndex = 0;
+            // 
+            // findBtn
+            // 
+            this.findBtn.BackColor = System.Drawing.Color.MistyRose;
+            this.findBtn.BackgroundImage = global::Group5Project.Properties.Resources.find;
+            this.findBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.findBtn.FlatAppearance.BorderColor = System.Drawing.Color.MistyRose;
+            this.findBtn.FlatAppearance.BorderSize = 0;
+            this.findBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.findBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.findBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.findBtn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.findBtn.ForeColor = System.Drawing.Color.Red;
+            this.findBtn.Location = new System.Drawing.Point(268, 24);
+            this.findBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.findBtn.Name = "findBtn";
+            this.findBtn.Size = new System.Drawing.Size(131, 40);
+            this.findBtn.TabIndex = 1;
+            this.findBtn.UseVisualStyleBackColor = false;
+            this.findBtn.Click += new System.EventHandler(this.findBtn_Click);
+            // 
+            // listMSSV
+            // 
+            this.listMSSV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(210)))), ((int)(((byte)(210)))));
+            this.listMSSV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.listMSSV.Font = new System.Drawing.Font("Arial", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listMSSV.FormattingEnabled = true;
+            this.listMSSV.Location = new System.Drawing.Point(15, 34);
+            this.listMSSV.Name = "listMSSV";
+            this.listMSSV.Size = new System.Drawing.Size(250, 29);
+            this.listMSSV.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(1382, 513);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QUẢN LÝ SINH VIÊN";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button findBtn;
+        private System.Windows.Forms.ComboBox listMSSV;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNgaysinh;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDiemtb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGioitinh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMakhoa;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Group5Project/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group5Project
+{
+    public partial class Form1 : Form
+    {
+        public static DataSet dsSV = new DataSet("SVDB");
+        public static SqlConnection con = new SqlConnection(@"uid=sa;pwd=blackpink9999;
+                                                              Initial Catalog=QLSVien;Data Source=SE141080\SQLEXPRESS");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadData();
+        }
+
+        private void loadData ()
+        {
+            string str = @"uid=sa;pwd=blackpink9999;Initial Catalog=QLSVien;Data Source=SE141080\SQLEXPRESS";
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM SVIEN", str);
+
+            dataAdapter.Fill(dsSV, "SVIEN");
+
+            for (int i = 0; i < dsSV.Tables["SVIEN"].Rows.Count; i++)
+            {
+                listMSSV.Items.Add(dsSV.Tables["SVIEN"].Rows[i]["MASV"]);
+            }
+
+            listMSSV.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void findBtn_Click(object sender, EventArgs e)
+        {
+            string findMSV = listMSSV.Text;
+
+            bool foundSV = false;
+
+            for (int i = 0; i < dsSV.Tables["SVIEN"].Rows.Count; i++)
+            {
+                if (findMSV.Equals(dsSV.Tables["SVIEN"].Rows[i]["MASV"].ToString()))
+                {
+                    txtHo.Text = dsSV.Tables["SVIEN"].Rows[i]["HOSV"].ToString();
+                    txtTen.Text = dsSV.Tables["SVIEN"].Rows[i]["TEN"].ToString();
+                    txtNgaysinh.Text = dsSV.Tables["SVIEN"].Rows[i]["NGAYSINH"].ToString();
+                    txtGioitinh.Text = dsSV.Tables["SVIEN"].Rows[i]["GIOITINH"].ToString();
+                    txtMakhoa.Text = dsSV.Tables["SVIEN"].Rows[i]["MAKH"].ToString();
+                    foundSV = true;
+                    break;
+                }
+                else
+                {
+                    foundSV = false;
+                }
+            }
+
+            if (foundSV == false)
+            {
+                txtHo.Text = "";
+                txtTen.Text = "";
+                txtNgaysinh.Text = "";
+                txtGioitinh.Text = "";
+                txtMakhoa.Text = "";
+                txtDiemtb.Text = "";
+                if (dsSV.Tables.Contains("MHOC"))
+                {
+                    dsSV.Tables.Remove("MHOC");
+                }
+                this.dataGridView1.DataSource = dsSV.Tables["MHOC"];
+                this.dataGridView1.Refresh();
+                this.dataGridView1.Parent.Refresh();
+                MessageBox.Show("NOT FOUND!");
+                return;
+            }
+
+            txtHo.ReadOnly = true;
+            txtTen.ReadOnly = true;
+            txtNgaysinh.ReadOnly = true;
+            txtGioitinh.ReadOnly = true;
+            txtMakhoa.ReadOnly = true;
+            txtDiemtb.ReadOnly = true;
+
+            string str = @"uid=sa;pwd=blackpink9999;Initial Catalog=QLSVien;Data Source=SE141080\SQLEXPRESS";
+
+            string sql = string.Format("SELECT t.MAMH AS 'MÃ MH', m.TENMH AS 'TÊN MN', t.DIEM AS 'ĐIỂM' "
+                                     + "FROM MHOC AS m, (SELECT h.MAMH, h.MAHP, k.MASV, k.DIEM "
+                                                      + "FROM HPHAN AS h, (SELECT MASV, MAHP, DIEM "
+                                                                        + "FROM KQUA "
+                                                                        + "WHERE MASV = {0}) AS k "
+                                                                        + "WHERE h.MAHP = k.MAHP) AS t "
+                                     + "WHERE m.MAMH = t.MAMH", findMSV);
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sql, str);
+
+            if (dsSV.Tables.Contains("MHOC"))
+            {
+                dsSV.Tables.Remove("MHOC");
+            }
+            
+            dataAdapter.Fill(dsSV, "MHOC");
+            dataAdapter.Update(dsSV.Tables["MHOC"]);
+
+            int totalRecord = dsSV.Tables["MHOC"].Rows.Count;
+
+            float totalMark = 0;
+
+            for (int j = 0; j < totalRecord; j++)
+            {
+                totalMark += float.Parse(dsSV.Tables["MHOC"].Rows[j]["ĐIỂM"].ToString());
+            }
+
+            float averageMark = totalMark / totalRecord;
+
+            this.dataGridView1.DataSource = dsSV.Tables["MHOC"];
+            this.dataGridView1.Refresh();
+            this.dataGridView1.Parent.Refresh();
+
+            if (averageMark.ToString().Equals("NaN"))
+            {
+                txtDiemtb.Text = "";
+            }
+            else
+            {
+                txtDiemtb.Text = averageMark.ToString();
+            }
+            
+        }
+    }
+}
