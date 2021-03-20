9947e0eaec292a0af0de63e1a1848980b6d762fe
hieuttblackpink <trantronghieu369963@gmail.com>
2021-03-20 14:28:34 +0700

    V04

---
+++ Group5Project/DBSQLServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group5Project
+{
+    class DBSQLServer
+    {
+        public static SqlConnection GetSqlConnection ()
+        {
+            string str = @"uid=sa;pwd=blackpink9999;Initial Catalog=QLSVien;Data Source=SE141080\SQLEXPRESS";
+
+            SqlConnection con = new SqlConnection(str);
+
+            return con;
+        }
+    }
+}
